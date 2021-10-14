7cc72c82a2ffe05048db90421ae3ec6fc32ee4f0
SHAIK AFRID <88675474+afridanon@users.noreply.github.com>
2021-10-14 03:26:59 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,19 @@
+import os
 import shutil
 import sys
+import time
+from colorama import Fore
+
+
+
+
+
+
+
 if shutil.which('git'):
+    print(Fore.YELLOW +"UPDATING....")
     os.system('git checkout . && git pull')
-    print('\n\t\tUpdated Successfull !!!!')
-    print('\tPlease Run The Script Again...')
+    print(Fore.YELLOW + "UPDATED SUCCESSFULLY RUN THE TOOL AGAIN ")
 else:
     print("Please reclone TBomb Again")
     sys.exit()
