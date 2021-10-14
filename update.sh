3817c21b02f78d525537d1ccfc25ffb74cbf9da1
SHAIK AFRID <88675474+afridanon@users.noreply.github.com>
2021-10-14 02:49:35 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 cd ..
 rm -rf wordlist_creator
-git clone https://github.com/afridanon/wordlist_creator
+git clone https://github.com/afridanon/wordlist_creator > /dev/null 2>&1
 cd wordlist_creator
 echo -e "\e[92m[\e[91m~\e[92m]\e[93m UPDATED SUCESSFULLY !\e[93m"
 sleep 4
