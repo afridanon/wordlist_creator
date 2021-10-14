4f1d04ccedb178e494794f669ef6a1f386a6f385
SHAIK AFRID <88675474+afridanon@users.noreply.github.com>
2021-10-14 03:22:45 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,8 +1,9 @@
 import shutil
+import sys
 if shutil.which('git'):
     os.system('git checkout . && git pull')
     print('\n\t\tUpdated Successfull !!!!')
     print('\tPlease Run The Script Again...')
 else:
-  print("Please reclone TBomb Again")
-   sys.exit()
+    print("Please reclone TBomb Again")
+    sys.exit()
