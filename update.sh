428da994fae7116eba86ee69888ac09f9547b5cd
SHAIK AFRID <88675474+afridanon@users.noreply.github.com>
2021-10-14 03:14:47 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,32 @@
+clear
+echo -e "\e[92m "
+clear
+echo ""
+echo "       $CYAN█░█ █▀█ █▀▄ ▄▀█ ▀█▀ █ █▄░█ █▀▀   █▀ █▀▀ █▀█ █ █▀█ ▀█▀"
+echo "      $WHITE█▄█ █▀▀ █▄▀ █▀█ ░█░ █ █░▀█ █▄█   ▄█ █▄▄ █▀▄ █ █▀▀ ░█░"
+echo ""
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[36m Requesting Update From Source...\e[93m"
+sleep 2
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[0m Request Attempt Successful..\e[93m"
+sleep 1
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[36m Updating Now...\e[93m"
+sleep 2
+echo ""
 cd ..
 rm -rf wordlist_creator
 git clone https://github.com/afridanon/wordlist_creator > /dev/null 2>&1
 cd XLR8_BOMBER
+chmod +x xlr8.sh
+clear
+echo ""
+echo "       $GREEN █▀ █▀▀ █▀█ █ █▀█ ▀█▀   █░█ █▀█ █▀▄ ▄▀█ ▀█▀ █▀▀ █▀▄"
+echo "       $WHITE ▄█ █▄▄ █▀▄ █ █▀▀ ░█░   █▄█ █▀▀ █▄▀ █▀█ ░█░ ██▄ █▄▀ "
+echo ""
+sleep 3
+echo ""
+echo -e "        \e[92m[\e[91m~\e[92m]\e[91m Restarting XLR8 !\e[93m"
+sleep 2
 python wordlist.py
-
-
-
-echo -e "\e[92m[\e[91m~\e[92m]\e[93m UPDATED SUCESSFULLY !\e[93m"
-cd 
-sleep 4
