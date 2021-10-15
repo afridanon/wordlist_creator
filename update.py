3a3f0c9ca88ab89f5259d2ec9664b2076ee294b1
SHAIK AFRID <88675474+afridanon@users.noreply.github.com>
2021-10-15 11:26:43 -0400

    Update update.py

--- update.py
+++ update.py
@@ -12,16 +12,18 @@ def modules():
     from colorama import Fore
 
 
-if shutil.which('git'):
-    print(Fore.YELLOW +" -->>   UPDATING THE SCRIPT   <<--")
-    os.system('git checkout . && git pull')
-    print(Fore.YELLOW + " ->  UPDATED SUCCESSFULLY RUN THE TOOL AGAIN  <-")
-    time.sleep(4)
-    os.system("python wordlist.py")
-else:
-    print("Please reclone wordlist_creator Again")
-    sys.exit()
+
 if __name__ == "__main__":
     depe()
+    modules()
+    if shutil.which('git'):
+        print(Fore.YELLOW +" -->>   UPDATING THE SCRIPT   <<--")
+        os.system('git checkout . && git pull')
+        print(Fore.YELLOW + " ->  UPDATED SUCCESSFULLY RUN THE TOOL AGAIN  <-")
+        time.sleep(4)
+        os.system("python wordlist.py")
+    else:
+        print("Please reclone wordlist_creator Again")
+        sys.exit()
 
     
